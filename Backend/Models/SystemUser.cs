27a0d2a23e4da15de31c03566e5351dd7478013f
Rex <jcadiao1102@gmail.com>
2022-09-10 12:51:23 +0800

    Init

---
+++ Backend/Models/SystemUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models;
+
+public class SystemUser
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
