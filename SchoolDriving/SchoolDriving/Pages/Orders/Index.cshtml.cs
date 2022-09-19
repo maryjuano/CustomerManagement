30d74d3ffa99ecd42a291acf50ace5ef46cbbcf1
Rex <jcadiao1102@gmail.com>
2022-09-19 16:19:05 +0800

    Dashboard

--- SchoolDriving/SchoolDriving/Pages/Orders/Index.cshtml.cs
+++ SchoolDriving/SchoolDriving/Pages/Orders/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using SchoolDriving.Models;
 
 namespace SchoolDriving.Pages.Orders
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly SchoolDriving.Data.ApplicationDbContext _context;
