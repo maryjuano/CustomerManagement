ffa3c0c10acd0f52321fd0545738058cacc013b7
Rex <jcadiao1102@gmail.com>
2022-09-19 16:46:40 +0800

    Users

---
+++ SchoolDriving/SchoolDriving/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SchoolDriving.Data;
+using SchoolDriving.Models;
+
+namespace SchoolDriving.Pages.Users
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolDriving.Data.ApplicationDbContext _context;
+
+        public IndexModel(SchoolDriving.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<IdentityUser> Users { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Users != null)
+            {
+                Users = await _context.Users.ToListAsync();
+            }
+        }
+    }
+}
